9e26574d1d3dec19cbb501ba7d67379730429b9f
Johan Hörnqvist <johhor@watcnoclosxl005.(none)>
2012-02-19 00:29:52 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gympamanager::Application.config.secret_token = '974c45f426e15a2e306fcd47e88dcbbba349bb6bdda4110714aa8bfb917543eac7071e601a223d50ee819eabbda25dac9249ec0984d04c841df45a2870354d67'
